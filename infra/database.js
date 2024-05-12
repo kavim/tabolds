20b61143878ef44cf83627af89dd1d1b26727d53
kavim <kevinmmti@gmail.com>
2024-05-12 09:21:53 -0300

    Docker: Postgres config

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
