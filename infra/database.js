cd8c725e298d074b5bcaecee3526f76c3b222ed0
kevinMMedeiros <kevin@cambioreal.com>
2024-06-15 19:04:12 -0300

    make  more robust to catch errors and prevent sql injection

--- infra/database.js
+++ infra/database.js
@@ -2,18 +2,48 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
   });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  client.connect();
+
+  try {
+    return await client.query(queryObject);
+  } catch (error) {
+    console.error("Error running query", error);
+  } finally {
+    await client.end();
+  }
+}
+
+async function info() {
+  let max_connections = await query(
+    "SELECT sum(numbackends) FROM pg_stat_database;",
+  );
+
+  let version = await query("SHOW server_version;");
+  let dbName = process.env.POSTGRES_USER;
+  let openedConnections = await query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+
+  let dbinfo = {
+    status: "ok",
+    database: "postgres",
+    version: version.rows[0].server_version,
+    max_connections: parseInt(max_connections.rows[0].sum),
+    openedConnections: openedConnections.rows[0].count,
+  };
+
+  return dbinfo;
 }
 
 export default {
   query: query,
+  info: info,
 };
